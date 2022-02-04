f3e3dbeed15ffdce6e8a6b29f8b180af925e2bfd
niwantha33 <niwantha33@gmail.com>
2022-02-05 00:01:08 +0530

    Thinger Updated.

--- include/arduino_secrets.h
+++ include/arduino_secrets.h
@@ -2,5 +2,5 @@
 #define DEVICE_ID "lionbit"
 #define DEVICE_CREDENTIAL "BllK4JgF-q_LckAL"
 
-#define SSID "King-knight"
-#define SSID_PASSWORD "nrl1633n"
+#define SSID "King-"
+#define SSID_PASSWORD "......"
